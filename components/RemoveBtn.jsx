ee0053d0c5e1fb7cbb928d425b6432b1ab563b30
MG649506 <106833450+MG649506@users.noreply.github.com>
2023-11-28 19:20:21 -0700

    Initial commit

---
+++ components/RemoveBtn.jsx
@@ -0,0 +1,32 @@
+'use client';
+import React from 'react';
+import {HiTrash} from "react-icons/hi";
+import { useRouter } from "next/navigation";
+
+export default function RemoveBtn({id}){
+
+const router = useRouter();
+
+const removeTopic = async () => {
+    
+    const confirmed = confirm("Are you sure you want to delete this topic?");
+
+if (confirmed){
+    const res = await fetch(`http://localhost:3000/api/topics?id=${id}`,{
+        method: "DELETE",
+});
+if(res.ok){
+    router.refresh();
+}
+
+
+}
+
+};
+
+    return(
+       <button onClick={removeTopic} className='text-red-600'>
+        <HiTrash size={24}/>
+       </button>
+    );
+}
